7c2d9de36c20998775c84f3e9850986db3def6cd
Samyak Jain <samyak.jn2431@gmail.com>
2025-02-24 11:18:16 +0530

    minor change

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
